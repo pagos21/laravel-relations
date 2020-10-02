fc15954fe300e6b85cf0c3e1c75f8a1bb60ba84f
pagos21 <paga@localhost.yeah>
2020-10-02 18:08:56 +0200

    v 2.2.1(final fix)

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,7 +1,8 @@
 
 <h3 id="title_show">REPO:laravel-hotel-cruds
   <div class="hidden">
-    <h3>GOAL: aggiungere alla repo precedente quanto visto questa mattina a lezione, ovvero index + update + delete sull'enetita' pagamenti
+    <h3>GOAL: come detto a lezione, in aggiunta alla repo di ieri, riportare CRUD completa su entita' Employee lato front-end; prestare particolare attenzione ai form (create/update) e ricordarsi di aggiungere la onDelete('cascade') nella migration delle relazione (se non vi ricordate dove, date un occhio al video ;-)) per permettervi di eliminare un Employee
+
 </h3>
   </div>
 </h3>
